845e5f744f82ded895c849105405f6dce37ceeec
loryu <chinaloryu@gmail.com>
2017-07-20 19:25:17 +0800

    @20170720 add distance sensor

--- lcdDisplay/lcdDisplay/lcdDisplay.ino
+++ lcdDisplay/lcdDisplay/lcdDisplay.ino
@@ -14,27 +14,34 @@
    http://www.arduino.cc/en/Tutorial/LiquidCrystal
    */
    
-  // 引用LiquidCrystal Library
   #include <LiquidCrystal.h>
    
-  // 建立LiquidCrystal 的变数lcd
-  // LCD 接脚: rs, enable, d4, d5, d6, d7 
-  // 对应到Arduino 接脚: 12, 11, 5, 4, 3, 2
   LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  int inputPin=8;
+  int outputPin=9;
    
   void setup() {
-    // 设定LCD 的行列数目(2 x 16)
     lcd.begin(16, 2);
-   
-    // 列印"Hello World" 讯息到LCD 上
-    lcd.print("hello, world!");
+    lcd.print("Distance:");
+
+    Serial.begin(9600);
+    pinMode(inputPin, INPUT);
+    pinMode(outputPin, OUTPUT);
   }
    
   void loop() {
-    // 将游标设到column 0, line 1
-    // (注意: line 1 是第二行(row)，因为是从0 开始数起):
+
+    digitalWrite(outputPin, LOW); 
+    delayMicroseconds(2);
+    digitalWrite(outputPin, HIGH); 
+    delayMicroseconds(10);
+    digitalWrite(outputPin, LOW); 
+    int distance = pulseIn(inputPin, HIGH); 
+    distance= distance/58; 
+    Serial.println(distance); 
+    delay(50); 
+
     lcd.setCursor(0, 1);
    
-    // 列印Arduino 重开之后经过的秒数
-    lcd.print(millis()/1000);
+    lcd.print(distance);
   }
