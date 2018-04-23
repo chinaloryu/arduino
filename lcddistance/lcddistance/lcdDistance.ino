cbd4333a53b9d545b3d87a77ed37a3e2760470e3
loryu <chinaloryu@gmail.com>
2018-04-24 07:44:54 +0800

    @20180424

--- lcddistance/lcddistance/lcdDistance.ino
+++ lcddistance/lcddistance/lcdDistance.ino
@@ -18,6 +18,8 @@
   Servo myservo;  // create servo object to control a servo
   // twelve servo objects can be created on most boards
   int pos = 0;    // variable to store the servo position
+
+  int distance;
   
   LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
   int inputPin=7;
@@ -31,42 +33,33 @@
     
     myservo.attach(9);
   }
-   
+  void write2lcd()
+  {  
+    digitalWrite(outputPin, LOW); 
+    delayMicroseconds(2);
+    digitalWrite(outputPin, HIGH); 
+    delayMicroseconds(10);
+    digitalWrite(outputPin, LOW); 
+    distance = pulseIn(inputPin, HIGH); 
+    distance= distance/58; 
+    lcd.setCursor(0,0);
+    lcd.print("Distance:");
+    lcd.setCursor(0, 1);
+    lcd.print(distance);
+    delay(1000); 
+    lcd.clear();  
+  }  
   void loop() {
-    int distance;
+
     for (pos = 0; pos <= 180; pos += 10) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(200);                       // waits 15ms for the servo to reach the position
-      digitalWrite(outputPin, LOW); 
-      delayMicroseconds(2);
-      digitalWrite(outputPin, HIGH); 
-      delayMicroseconds(10);
-      digitalWrite(outputPin, LOW); 
-      distance = pulseIn(inputPin, HIGH); 
-      distance= distance/58; 
-      lcd.setCursor(0,0);
-      lcd.print("Distance:");
-      lcd.setCursor(0, 1);
-      lcd.print(distance);
-      delay(50); 
-      lcd.clear();
+      delay(20);                       // waits 15ms for the servo to reach the position
+      write2lcd();
     }
     for (pos = 180; pos >= 0; pos -= 10) { // goes from 180 degrees to 0 degrees
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(200);                       // waits 15ms for the servo to reach the position
-      digitalWrite(outputPin, LOW); 
-      delayMicroseconds(2);
-      digitalWrite(outputPin, HIGH); 
-      delayMicroseconds(10);
-      digitalWrite(outputPin, LOW); 
-      distance = pulseIn(inputPin, HIGH); 
-      distance= distance/58; 
-      lcd.setCursor(0,0);
-      lcd.print("Distance:");
-      lcd.setCursor(0, 1);
-      lcd.print(distance);
-      delay(50); 
-      lcd.clear();
+      delay(20);                       // waits 15ms for the servo to reach the position
+      write2lcd();
     }
   }
